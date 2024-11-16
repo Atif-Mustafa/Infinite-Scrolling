4bd8e03053d5b525f2645afb49681f853584320f
Atif Mustafa <atif.mustafa0010@gmail.com>
2024-11-16 23:58:13 +0530

    task done

--- script.js
+++ script.js
@@ -14,17 +14,50 @@ async function getPhotos(){
 
     data.forEach(photo => {
         try{
+            let div = document.createElement("div");
+            div.classList.add('imgC');
+
+
+            let tc = document.createElement("div");
             let img = document.createElement("img");
             img.src = photo.urls.regular;
-            img.style.height = "200px";
-            img.style.width = "200px";
-            img.style.marginBottom = "200px";
+            img.classList.add('decorate');
+            tc.appendChild(img);
+            
+            let bc = document.createElement("div");
+            let lc = document.createElement("span");
+            let likes = document.createElement("span");
+            likes.innerHTML = `<i class="fa-regular fa-thumbs-up"></i>`;
+            let heart = document.createElement("span");
+            heart.innerHTML = `<i class="fa-regular fa-heart fa-shake"></i>`
+            
+            lc.appendChild(likes);
+            lc.appendChild(heart);
 
-            imageContainer.appendChild(img);
+            likes.style.marginInline = ".5rem";
+
+            bc.appendChild(lc);
+            bc.classList.add("bc")
+            
+            let rc = document.createElement("span");
+            let views = document.createElement("p");
+            views.innerText = photo.views;
+            rc.appendChild(views);
+            bc.appendChild(rc);
+
+            
+            
+            
+            img.addEventListener("click", () => {
+                
+            })
+
+            div.appendChild(tc);
+            div.appendChild(bc);        
+            imageContainer.appendChild(div);
         }
         catch(error){
-            console.log(error);
-            
+            console.log(error);            
         }
     });
 
